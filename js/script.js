5af76424ab5ba03833d6095319727126fa18730a
OMathiaslopez <mathiasmellolz17@gmail.com>
2024-09-04 11:31:35 -0300

    0409

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-data = new Date();
+// data = new Date();
 // alert (`Horal local:${dat.getHours()}`);
 // alert (`Horal universal:${data.getUTCHours()}`);
-dias=["domingo","segunda","terça","qurta",
-"quinta","sexta","sabado"];
-alert(data.getHours());
-alert(data.getMinutes());
-alert(data.getMilliseconds());
-alert(data.getDate());
-alert(dias[data.getDay()]);
-alert(data.getMonth());
-alert(data.getFullYear());
+// dias=["domingo","segunda","terça","qurta",
+// "quinta","sexta","sabado"];
+// alert(data.getHours());
+// alert(data.getMinutes());
+// alert(data.getMilliseconds());
+// alert(data.getDate());
+// alert(dias[data.getDay()]);
+// alert(data.getMonth());
+// alert(data.getFullYear());    
+ 
+// alert(Math.PI);
+// alert(Math.abs(-5.9));
+// alaert(Math.max(5.9, 6, 12, -80));
+// alert(Math.round(12.6))
+// alert(Math.floor(45.9));
+// alert(Math.ceil(45.1));
+// alert(Math.pow(12, 3));
+// alert(Math.sqrt(16));
+
+alert(Math.floor(Math.random()*6+1))
+
